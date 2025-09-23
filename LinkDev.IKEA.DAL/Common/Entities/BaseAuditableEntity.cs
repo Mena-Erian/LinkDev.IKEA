809ec7a46fffb9b8c3a587ce169e0d78c77234aa
Mena Erian <mena3koko2karas1@gmail.com>
2025-09-23 18:37:50 +0300

    Part 03 Department Module, Part 04 Base Entities, Part 05 Department Module (Entities)

---
+++ LinkDev.IKEA.DAL/Common/Entities/BaseAuditableEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Common.Entities
+{
+    internal class BaseAuditableEntity<TKey> : BaseEntity<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public required string LastModifiedBy { get; set; }
+    }
+}
---
+++ LinkDev.IKEA.DAL/Common/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Common.Entities
+{
+    internal class BaseEntity<TKey> where TKey : IEquatable<TKey>
+    {
+        public TKey Id { get; set; }
+
+    }
+}
