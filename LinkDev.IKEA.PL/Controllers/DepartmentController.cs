14f605bfad4df09f77b8dcf0eb1e5bb5ba8ccadb
Mena Erian <mena3koko2karas1@gmail.com>
2025-09-29 17:23:37 +0300

    Part 05 Department Controller

---
+++ LinkDev.IKEA.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,28 @@
+﻿using LinkDev.IKEA.BLL.Services.Departments;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinkDev.IKEA.PL.Controllers
+{
+    // Inheritance: DepartmentController is a Controller
+    // Composition(not aggregation): DepartmentController has a IDepartmentService
+    public class DepartmentController : Controller
+    {
+        #region Services
+        private readonly IDepartmentService _departmentService;
+
+        //Used if i have just some action need to this service not all actions
+        ///[FromServices]
+        ///public IDepartmentService DepartmentService { get; set; }
+
+        public DepartmentController(IDepartmentService departmentService) // Ask Runtime for Creating an Instance from 
+        {
+            _departmentService = departmentService;
+        }
+        #endregion
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
