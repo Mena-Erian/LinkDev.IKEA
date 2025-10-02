97f8b1a492f6dc7bc8881f7820a7733cd208bc97
Mena Erian <mena3koko2karas1@gmail.com>
2025-10-02 12:24:22 +0300

    Employee Entity and Configurations

---
+++ LinkDev.IKEA.DAL/Persistence/Data/Configurations/Employees/EmployeeConfigurations.cs
@@ -0,0 +1,37 @@
+﻿using LinkDev.IKEA.DAL.Common.Enums;
+using LinkDev.IKEA.DAL.Entities.Employees;
+using LinkDev.IKEA.DAL.Persistence.Data.Configurations.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Persistence.Data.Configurations.Employees
+{
+    public class EmployeeConfigurations : BaseAuditableEntityConfigurations<int, Employee>
+    {
+        public override void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(e => e.Id).UseIdentityColumn(1, 1);
+            builder.Property(e => e.FirstName).HasColumnType("varchar(50)").IsRequired();
+            builder.Property(e => e.LastModifiedBy).HasColumnType("varchar(50)").IsRequired();
+            builder.Property(e => e.Email).HasColumnType("varchar(100)");
+
+            builder.Property(e => e.Gender).HasConversion(
+                (gender) => gender.ToString(),
+                (gender) => Enum.Parse<Gender>(gender)
+            );
+
+            builder.Property(e => e.EmployeeType).HasConversion(
+                    empType => empType.ToString(),
+                    empType => Enum.Parse<EmployeeType>(empType)
+                );
+
+        }
+    }
+}
