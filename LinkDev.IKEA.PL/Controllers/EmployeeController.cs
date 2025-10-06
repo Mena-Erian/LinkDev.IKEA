46a54166e776e1513bc458f0b5e3f5cb7de19b2c
Mena Erian <mena3koko2karas1@gmail.com>
2025-10-06 20:01:38 +0300

    Employee Controller - Index

---
+++ LinkDev.IKEA.PL/Controllers/EmployeeController.cs
@@ -0,0 +1,65 @@
+﻿using LinkDev.IKEA.BLL.Services.Employees;
+using LinkDev.IKEA.DAL.Entities.Employees;
+using LinkDev.IKEA.DAL.Persistence.Common;
+using LinkDev.IKEA.PL.ViewModels.Employees;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinkDev.IKEA.PL.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(IEmployeeService employeeService, ILogger<EmployeeController> logger)
+        {
+            _employeeService = employeeService;
+            _logger = logger;
+        }
+
+        [HttpGet]   // GET: /EmployeeViewModel/Index
+        public IActionResult Index(int pageIndex = 1, int pageSize = 10)
+        {
+            var queryParameters = new QueryParameters()
+            {
+                PageIndex = pageIndex,
+                PageSize = pageSize
+            };
+
+
+            var employees = _employeeService.GetEmployees(queryParameters);
+
+            var model = new EmployeeListViewModel()
+            {
+                Employees = employees.Data.Select(emp => new EmployeeViewModel()
+                {
+                    Id = emp.Id,
+                    FullName = $"{emp.FirstName} {emp.LastName}",
+                    Email = emp.Email ?? string.Empty,
+                    PhoneNumber = emp.PhoneNumber ?? string.Empty,
+                    Address = emp.Address ?? string.Empty,
+                    Salary = emp.Salary,
+                    IsActive = emp.IsActive,
+                    Age = emp.Age ?? default,
+                    FormattedHireDate = emp.HireDate.ToString(),
+                    Gender = emp.Gender,
+                    EmployeeType = emp.EmployeeType,
+                    Department = emp.DepartmentId.ToString() ?? string.Empty,
+                    CreatedBy = emp.CreatedBy,
+                    CreatedOn = emp.CreatedOn,
+                    LastModifiedBy = emp.LastModifiedBy,
+                    LastModifiedOn = emp.LastModifiedOn
+                }),
+                Page = employees.PageIndex,
+                PageSize = employees.PageSize,
+                TotalCount = employees.TotalPageCount,
+            };
+
+            return View(model);
+        }
+    }
+}
+
+/// PageIndex = employees.PageIndex,
+/// PageSize = employees.PageSize,
+/// TotalCount = employees.TotalCount,
