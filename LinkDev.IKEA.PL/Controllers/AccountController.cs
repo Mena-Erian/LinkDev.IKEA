d23fef26dc4819922798135fdd097045632d34e2
Mena Erian <mena3koko2karas1@gmail.com>
2025-10-13 18:56:24 +0300

    Account Controller - ResetPassword

--- LinkDev.IKEA.PL/Controllers/AccountController.cs
+++ LinkDev.IKEA.PL/Controllers/AccountController.cs
@@ -160,10 +160,7 @@ namespace LinkDev.IKEA.PL.Controllers
         #region Forget Password
 
         [HttpGet]
-        public IActionResult ForgetPassword()
-        {
-            return View();
-        }
+        public IActionResult ForgetPassword() => View();
 
         [HttpPost]
         public IActionResult SendResetPasswordUrl(ForgetPasswordViewModel model)
@@ -174,7 +171,8 @@ namespace LinkDev.IKEA.PL.Controllers
                 if (user is not null)
                 {
                     var token = _userManager.GeneratePasswordResetTokenAsync(user).GetAwaiter().GetResult();
-                    var url = Url.Action(nameof(ForgetPassword), "Account",
+                    
+                    var url = Url.Action(nameof(ResetPassword), "Account",
                                          new
                                          {
                                              model.Email,
@@ -197,7 +195,7 @@ namespace LinkDev.IKEA.PL.Controllers
                 }
                 else
                 {
-                    ModelState.AddModelError("", "Invalid Opeartion Please Try Again");
+                    ModelState.AddModelError("", "Invalid Operation Please Try Again");
                 }
             }
 
@@ -206,6 +204,40 @@ namespace LinkDev.IKEA.PL.Controllers
 
         [HttpGet]
         public IActionResult CheckYourInbox() => View();
+
+        [HttpGet]
+        public IActionResult ResetPassword(string email, string token)
+        {
+            TempData["email"] = email;
+            TempData["token"] = token;
+            return View();
+            //Pass email, Token
+        }
+
+        [HttpPost]
+        public IActionResult ResetPassword(ResetPasswordViewModel resetPasswordViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var email = TempData["email"] as string ?? "";
+                var token = TempData["token"] as string ?? "";
+
+                var user = _userManager.FindByEmailAsync(email).Result;//Sync
+
+                if (user != null)
+                {
+                    var result = _userManager.ResetPasswordAsync(user, token, resetPasswordViewModel.NewPassword).Result;
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction(nameof(SignIn));
+                    }
+                }
+
+            }
+
+            ModelState.AddModelError("", "Invalid Operation, Please Try Again");
+            return View(resetPasswordViewModel);
+        }
         #endregion
 
     }
